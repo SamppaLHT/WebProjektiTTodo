2b9dd6952d0978179bb7d6f18787f2a0e82edec0
Samu <samu.lyhty@gmail.com>
2025-09-06 19:40:18 +0300

    part 7 body implemented but not fully functional

--- src/screens/App.jsx
+++ src/screens/App.jsx
@@ -1,13 +1,15 @@
 import { useEffect, useState } from 'react'
-import './App.css'
+import '../App.css'
 import axios from 'axios'
-import Row from './components/Row'
+import Row from '../components/Row'
+import { useUser } from '../context/useUser.jsx'
 
 const url = "http://localhost:3001"
 
 function App() {
   const [task, setTask] = useState('')
   const [tasks, setTasks] = useState([])
+  const { user } = useUser()
 
   useEffect(() => {
     axios.get(url)
@@ -20,9 +22,10 @@ function App() {
   }, [])
 
   const addTask = () => {
+    const headers = {headers: {Authorization: user.token}}
     const newTask = { description: task }
 
-    axios.post(url + "/create", {task: newTask})
+    axios.post(url + "/create", {task: newTask}, headers)
     .then(response => {
       setTasks([...tasks, response.data])
       setTask('')
@@ -33,6 +36,8 @@ function App() {
   }
 
   const deleteTask = (deleted) => {
+    const headers = {headers: {Authorization: user.token}}
+    console.log(headers)
     axios.delete(url + "/delete/" + deleted)
     .then(response => {
       setTasks(tasks.filter(item => item.id !== deleted))
